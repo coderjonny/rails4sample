c07c9c24ba5c9efa0255aa91c05d8d7d10c9d29f
jonny kang <jono.kang@gmail.com>
2013-09-11 02:37:15 -0700

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,22 +8,6 @@ describe "StaticPages" do
     it { should have_title(full_title(page_title)) }
   end
 
-  describe "for signed-in users" do
-    let(:user) { FactoryGirl.create(:user) }
-    before do
-      FactoryGirl.create(:micropost, user: user, content: "lorem ipsum")
-      FactoryGirl.create(:micropost, user: user, content: "dolor sit amet")
-      sign_in user
-      visit root_path
-    end
-
-    it "should render the user's feed" do
-      user.feed.each do |item|
-        expect(page).to have_selector("li##{item.id}", text: item.content)
-      end
-    end
-  end
-
   describe "Home Page" do
     before { visit root_path }
     let(:heading)    { 'Sample App' }
@@ -31,6 +15,33 @@ describe "StaticPages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+    end
   end
 
   describe "Help page" do
